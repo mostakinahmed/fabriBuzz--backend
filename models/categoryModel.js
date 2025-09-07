3c58b5e905e5ef204d67fc95013234515fca5167
mostakinahmed <mostakin4556@gmail.com>
2025-09-07 15:32:31 +0600

    a

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   catID: {
     type: String,
-    required: [true, "Please enter category ID"],
+    //required: [true, "Please enter category ID"],
     trim: true,
     maxLength: [100, "Category ID cannot exceed 100 characters"],
   },
 
   catName: {
     type: String,
-    required: [true, "Please enter category name"],
+
+    //required: [true, "Please enter category name"],
   },
 });
 
