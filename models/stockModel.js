537e35fca395dca06d19c7ea3aadd58df672c0ba
Mostakin <mostakin4556@gmail.com>
2025-11-14 19:40:07 +0600

    update stock model

--- models/stockModel.js
+++ models/stockModel.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
-const skuSchema = new mongoose.Schema({
-  skuID: { type: String, required: true },
-  OID: { type: String, default: null },
-  comment: { type: String },
-  status: { type: Boolean, default: true },
-});
-
-const stockSchema = new mongoose.Schema(
+const skuSchema = new mongoose.Schema(
   {
-    sID: { type: String, required: true, trim: true, unique: true }, // optional: unique per stock
-    pID: { type: String, required: true, trim: true, unique: true }, // one stock per product
-    SKU: { type: [skuSchema], default: [] }, // multiple SKUs per stock
+    skuID: { type: String, required: true },
+    OID: { type: String, default: null },
+    comment: { type: String },
+    status: { type: Boolean, default: true },
   },
   { timestamps: true, versionKey: false }
 );
+
+const stockSchema = new mongoose.Schema({
+  sID: { type: String, required: true, trim: true, unique: true }, // optional: unique per stock
+  pID: { type: String, required: true, trim: true, unique: true }, // one stock per product
+  SKU: { type: [skuSchema], default: [] }, // multiple SKUs per stock
+});
 
 module.exports = mongoose.model("stock", stockSchema);
